ce5b38a70fcb83d48c88736c01f227e19dad6130
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2018-11-05 16:14:10 +0100

    Fix Linux setup

--- bash/set-vars-os.sh
+++ bash/set-vars-os.sh
@@ -15,33 +15,49 @@ export DOTFILES_OS_DIST=
 # - Version of Ubuntu: 14.04, 16.04, ...
 export DOTFILES_OS_VERS=
 
-# SET `DOT_OS_*` VARIABLES [TODO Fall back to uname?]
-case "$OSTYPE" in
-	linux*)
-	DOTFILES_OS=LINUX
-	if [ -f /etc/*-release ]; then
-		local DISTRIB_ID
-		local DISTRIB_RELEASE
-		source /etc/*-release
-		
-		# Reset variables set by the command
-		# (converted to upper case - requires bash 4+).
-		DOTFILES_OS_DIST="${DISTRIB_ID^^}"
-		DOTFILES_OS_VERS="${DISTRIB_RELEASE^^}"
-	fi
-	;;
-	darwin*)
-		DOTFILES_OS=OSX
-		;;
-	win*) # [TODO Verify]
-		DOTFILES_OS=WIN
-		;;
-	cygwin*) # [TODO Verify]
-		DOTFILES_OS=WIN
-		DOTFILES_OS_DIST=CYGWIN
-		;;
-	msys*) # [TODO Verify]
-		DOTFILES_OS=WIN
-		DOTFILES_OS_DIST=MINGW
-		;;
-esac
+_set_os_vars() {
+	local -r os="$1"
+	# SET `DOT_OS_*` VARIABLES [TODO Fall back to uname?]
+	case "$os" in
+		linux*)
+			DOTFILES_OS=LINUX
+			if [ -f /etc/os-release ]; then
+				local ID
+				local VERSION_ID
+				source /etc/lsb-release
+				
+				# Reset variables set by the command
+				# (converted to upper case - requires bash 4+).
+				DOTFILES_OS_DIST="${ID^^}"
+				DOTFILES_OS_VERS="${VERSION_ID^^}"
+			elif [ -f /etc/lsb-release ]; then
+				local DISTRIB_ID
+				local DISTRIB_RELEASE
+				source /etc/lsb-release
+				
+				# Reset variables set by the command
+				# (converted to upper case - requires bash 4+).
+				DOTFILES_OS_DIST="${DISTRIB_ID^^}"
+				DOTFILES_OS_VERS="${DISTRIB_RELEASE^^}"
+			fi
+			# TODO Support other (legacy) release files just for insanity's sake: http://linuxmafia.com/faq/Admin/release-files.html
+			;;
+		darwin*)
+			DOTFILES_OS=OSX
+			;;
+		win*) # [TODO Verify]
+			DOTFILES_OS=WIN
+			;;
+		cygwin*) # [TODO Verify]
+			DOTFILES_OS=WIN
+			DOTFILES_OS_DIST=CYGWIN
+			;;
+		msys*) # [TODO Verify]
+			DOTFILES_OS=WIN
+			DOTFILES_OS_DIST=MINGW
+			;;
+	esac
+}
+
+# OSTYPE is usually set by bash.
+_set_os_vars "$OSTYPE"
