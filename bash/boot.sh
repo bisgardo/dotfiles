082251d9aa9ba424f08df92276175c02815aa2fe
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2020-03-17 22:29:14 +0100

    Completion fix

--- bash/boot.sh
+++ bash/boot.sh
@@ -32,3 +32,13 @@ if [ "$DOTFILES_BOOTER" = 'BASHPROFILE' ]; then
 	export HISTFILESIZE=
 	export HISTSIZE=
 fi
+
+# TODO Move somewhere else.
+# enable bash completion in interactive shells
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+#  elif [ -f /etc/bash_completion ]; then
+#    . /etc/bash_completion
+  fi
+fi
