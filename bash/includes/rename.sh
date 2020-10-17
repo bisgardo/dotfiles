70d324ab3e92e8ce4bc461d96ec4b9e249362056
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2020-10-17 15:19:50 +0200

    Improve rename

--- bash/includes/rename.sh
+++ bash/includes/rename.sh
@@ -1,6 +1,14 @@
 # Rename file or directory relative to its current directory.
 rename() {
-	mv -i "$1" "$(dirname "$1")/$2"
+	if [ "$#" -ne 2 ]; then
+		>&2 echo 'Usage: rename PATH NEW_NAME'
+		return 1
+	fi
+	local path="$1"
+	local new_name="$2"
+	if [ -d "$path" ]; then
+		path="$(cd "$path" && pwd -P)" # resolve '.', '..', etc.
+	fi
+	mv -i "$path" "$(dirname "$path")/$new_name"
+	cd . # updates PWD if path of the dir itself changed
 }
-
-alias rn='rename'
