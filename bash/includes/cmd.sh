8b7d725070cdc63d6fcd6c2974fc4075045b8155
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-03-03 10:12:30 +0100

    Nvm and cmd stuff

--- bash/includes/cmd.sh
+++ bash/includes/cmd.sh
@@ -1,4 +1,4 @@
-export CMD_ROOTS="$HOME/cmds:$HOME/work/dev-scripts/cmd:$HOME/Documents/concordium-software/cmd"
+export CMD_ROOTS="$HOME/cmds:$HOME/work/mo-tools/cmd"
 
 alias ecmd='cmd --edit'
 alias lcmd='cmd --ls'
